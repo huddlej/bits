e665299ce8d7311cdd20f41f47e5d722bcf1874c
John Huddleston <huddlej@kexp.(none)>
2009-10-29 23:58:12 -0700

    added item class to manage per-item logic such as rendering

---
+++ _attachments/item.js
@@ -0,0 +1,24 @@
+function Item() {
+    this.render = function (template_name) {
+        console.log("Rendering item: " + this._id);
+        var context = {
+            id: this._id,
+            name: this.name,
+            condition: this.condition,
+            tags: this.tags || "&nbsp;"
+        };
+
+        if (this.checked_out_to == null) {
+            context.status = "in";
+            context.action_class = "checkout";
+            context.action_type = "check out";
+        }
+        else {
+            context.status = "out to " + this.checked_out_to;
+            context.action_class = "checkin";
+            context.action_type = "check in";
+        }
+
+        return $.tempest(template_name, context);
+    };
+}
